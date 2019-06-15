89c484ba98de0007a30fa0512c44b264a0242ef4
YetaWF <mikevdm@YetaWF.com>
2019-06-15 13:26:46 -0400

    copysite

---
+++ CopySite/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YetaWF Copy Site Utility")]
+[assembly: AssemblyDescription("YetaWF Copy Site Utility")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Softel vdm, Inc.")]
+[assembly: AssemblyProduct("CopySite")]
+[assembly: AssemblyCopyright("Copyright © 2019 - Softel vdm, Inc.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("5.0.0.0")]
+
+//[assembly: Package(PackageTypeEnum.Utility, "Softelvdm.CopySite.exe")]
+//[assembly: PackageInfo("https://YetaWF.com/UpdateServer",
+//    "https://yetawf.com/Documentation/YetaWF/CopySite",
+//    "https://YetaWF.com/Documentation/YetaWF/Support",
+//    "https://yetawf.com/Documentation/YetaWF/CopySite#Release%20Notice",
+//    "https://yetawf.com/Documentation/YetaWF/CopySite#License")]
