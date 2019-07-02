dd96a6a8bf32c324f92165ae9ba3550db9085bd9
YetaWF <mikevdm@YetaWF.com>
2019-07-02 12:53:04 -0400

    {bluegreen}

--- CopySite/Program.cs
+++ CopySite/Program.cs
@@ -307,12 +307,13 @@ namespace CopySite {
             foreach (string l in lines) {
                 string newLine = l;
                 if (BlueGreenDeploy != BlueGreenDeployEnum.None) {
+                    newLine = newLine.Replace("{bluegreen}", BlueGreenDeploy == BlueGreenDeployEnum.Blue ? "blue" : "green");
                     newLine = newLine.Replace("{BLUEGREEN}", BlueGreenDeploy == BlueGreenDeployEnum.Blue ? "Blue" : "Green");
                     newLine = newLine.Replace("{-BLUEGREEN}", BlueGreenDeploy == BlueGreenDeployEnum.Blue ? "-Blue" : "-Green");
                     newLine = newLine.Replace("{BLUEGREEN-OTHER}", BlueGreenDeploy == BlueGreenDeployEnum.Blue ? "Green" : "Blue");
                     newLine = newLine.Replace("{-BLUEGREEN-OTHER}", BlueGreenDeploy == BlueGreenDeployEnum.Blue ? "-Green" : "-Blue");
                 } else {
-                    if (l.Contains("{BLUEGREEN}") || l.Contains("{-BLUEGREEN}") || l.Contains("{BLUEGREEN-OTHER}") || l.Contains("{-BLUEGREEN-OTHER}"))
+                    if (l.Contains("{bluegreen}") || l.Contains("{BLUEGREEN}") || l.Contains("{-BLUEGREEN}") || l.Contains("{BLUEGREEN-OTHER}") || l.Contains("{-BLUEGREEN-OTHER}"))
                         throw new Error("BLUEGREEN variable found but this is not a blue-green deploy");
                 }
                 newLines.Add(newLine);
