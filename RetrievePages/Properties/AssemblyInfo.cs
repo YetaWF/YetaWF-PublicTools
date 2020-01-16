58d6df7335e5fe8a516da7c2455d65026c64bd0e
Mike <mikevdm@mikevdm.com>
2020-01-16 09:09:21 -0500

    2019 -> 2020 missed

--- RetrievePages/Properties/AssemblyInfo.cs
+++ RetrievePages/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RetrievePages")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2020 - Softel vdm, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
