cd95d623892a2491a0f906d50a152666922a5268
Mike <mikevdm@mikevdm.com>
2020-10-03 08:02:17 -0400

    deploy .well-known folder

--- DeploySite/Restore.cs
+++ DeploySite/Restore.cs
@@ -300,6 +300,7 @@ EXEC(@kill);";
             AddAllFilesToSite(Path.Combine("wwwroot", "AddonsCustom"));
             AddAllFilesToSite(Path.Combine("wwwroot", Program.MAINTENANCEFOLDER));
             AddAllFilesToSite(Path.Combine("wwwroot", "SiteFiles"));
+            AddAllFilesToSite(Path.Combine("wwwroot", ".well-known"), Optional: true);
             //AddAllFilesToSite(Path.Combine("wwwroot", "Vault"));
             AddFileToSite(Path.Combine("wwwroot", "logo.jpg"), Optional: true);
             AddFileToSite(Path.Combine("wwwroot", "robots.txt"));
